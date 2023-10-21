f2240d1f41fe0544ce421a681d53b40225b71541
Nishal Barman <helptechnoapps@gmail.com>
2023-10-21 14:52:25 +0530

    Replaced create-react-app with Vite for better performance

---
+++ frontend/src/Routes/AllRoutes.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../Components/homepage/HomePage";
+import ContactUs from "../Components/contact-us/ContactUs";
+import { Login } from "../Components/Login/Login";
+import { Signup } from "../Components/Signup/Signup";
+import Payment from "../Components/Payment/Payment";
+import Thankyou from "./../Components/Payment/Thankyou";
+import Hotel from "../Components/Hotels/Hotel";
+import { ChakraProvider } from "@chakra-ui/react";
+import HotelSingleInfo from "../Components/Hotels/HotelSingleInfo";
+
+function AllRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/contactus" element={<ContactUs />}></Route>
+      <Route path="/hotel" element={<Hotel />}></Route>
+      <Route path="/singlepage" element={<HotelSingleInfo />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/Signup" element={<Signup />}></Route>
+      <Route
+        path="/Payment"
+        element={
+          <ChakraProvider>
+            <Payment />
+          </ChakraProvider>
+        }></Route>
+      <Route
+        path="/Payment-Success"
+        element={
+          <ChakraProvider>
+            <Thankyou />
+          </ChakraProvider>
+        }></Route>
+    </Routes>
+  );
+}
+
+export default AllRoutes;
